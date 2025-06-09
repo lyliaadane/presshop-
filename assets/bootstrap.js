38dd013078b4faf6441dcd62c5ccfad5d947e7d9
damdam0305 <148003990+lyliaadane@users.noreply.github.com>
2025-06-09 21:22:42 +0200

    update

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,5 @@
-// assets/bootstrap.js
+import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-import { Application } from 'stimulus';
-import { definitionsFromContext } from 'stimulus/webpack-helpers';
-
-const application = Application.start();
-const context = require.context('./controllers', true, /\.js$/);
-application.load(definitionsFromContext(context));
+const app = startStimulusApp();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
